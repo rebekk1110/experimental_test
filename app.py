b2601ca111a9f751a4804af13b382131ac0c9d78
Rebekka Øvensen Aanderaa <125483437+rebekk1110@users.noreply.github.com>
2025-03-03 14:00:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -60,8 +60,5 @@ def register_participant():
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
-=======
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
->>>>>>> recovered-branch
+
